cf36ffaea2b434b7d4a666964b7db09690cd4261
sshadoan <shanna.shadoan@gmail.com>
2016-01-07 14:03:25 -0800

    Added input function

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,9 @@
-phrase = 'Hello, World!'
+phrase = 'Hello,'
 
-print(phrase)
+name=input("What is your name, please?") #make function equal a variable (name=)
+
+print(phrase, name) #recalls variable f/name, prints both
+
+
+
+
