d3ab8ad931133e4c60883a3fd80fdc2c57b1b967
sshadoan <shanna.shadoan@gmail.com>
2016-01-07 15:07:45 -0800

    ignoring some pyc

--- helloworld.py
+++ helloworld.py
@@ -2,7 +2,12 @@ phrase = 'Hello,'
 
 name=input("What is your name, please?") #make function equal a variable (name=)
 
+putput = phrase + ' '
+
+
 print(phrase, name) #recalls variable f/name, prints both
+
+
 
 
 
